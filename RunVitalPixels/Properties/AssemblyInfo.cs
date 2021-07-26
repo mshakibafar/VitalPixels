eaa29c168ad20d6842a88bb42245ddf5fd8c2ef0
mshakibafar <42954705+mshakibafar@users.noreply.github.com>
2021-07-26 11:50:02 +0430

    Property

---
+++ RunVitalPixels/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Vital Pixels Runtime")]
+[assembly: AssemblyDescription("Vital Pixels Runtime can run any plugin that was built in Vial Pixels IDE")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("VitalPixels")]
+[assembly: AssemblyProduct("VitalPixels Runtime")]
+[assembly: AssemblyCopyright("Copyright 2016 (C) VitalPixels")]
+[assembly: AssemblyTrademark("VitalPixels Runtime")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ea90f4ab-c084-4da5-8c8c-596de0f80e9e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
