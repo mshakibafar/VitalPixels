15bd3720104e69231157edab01be545cd9955f06
mshakibafar <42954705+mshakibafar@users.noreply.github.com>
2021-07-26 10:54:35 +0430

    Run

---
+++ RunVitalPixels/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RunVitalPixels
+{
+    class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] argv)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+            Application.Run(new MDIParentRunForm());
+
+
+        }
+    }
+}
